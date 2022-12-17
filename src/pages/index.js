652cd204ec1a3203a96099ab512f3945a0753519
tanasov49 <tanasov49@gmail.com>
2022-12-17 22:59:23 +0200

    add first lesson

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,58 @@
-import './index.css';
+// import './index.css';
+const valuesUl = document.querySelector('.select-body');
+const selectHeader = document.querySelector('.select-header');
+const selectBody = document.querySelector('.select-body');
+const selectCurrentHeader = document.querySelector('.select-header__current')
+const selectItem = document.querySelectorAll('.select-body__item')
+// Значения для массива select
+const values = [
+    {id: '1', value: 'Vue'},
+    {id: '2', value: 'React'},
+    {id: '3', value: 'NextJs'},
+    {id: '4', value: 'ExpressJs'},
+    {id: '5', value: 'Angular'}
+]
+// Вставка первого значения Value
+const selectHeaderText = document.querySelector('.select-header__current');
+selectHeaderText.textContent = values[0].value
+// Открытие Select Body
+const openSelectBody = (selectBody) => {
+    selectBody.classList.add('select-body_active');
+    selectCurrentHeader.classList.add('select-header__current_active');
+
+}
+// Закрытие Select Body
+const closeSelectBody = (selectBody) => {
+    selectBody.classList.remove('select-body_active');
+    selectCurrentHeader.classList.remove('select-header__current_active');
+
+}
+// Переменная для изменения состояния открытия selectBody
+let isSelectBodyShow = true;
+
+// Открытие и закрытие по клику Select
+selectHeader.addEventListener('click', ()=> {
+    if (isSelectBodyShow) {
+        openSelectBody(selectBody);
+        isSelectBodyShow = false;
+    } else {
+        closeSelectBody(selectBody);
+        isSelectBodyShow = true;
+    }
+})
+// Перебор и вставка массива из values
+values.forEach(function (value){
+    const valueText = document.createElement('li');
+    const idValue = value.id;
+    const textValue = value.value;
+    valueText.className = 'select-body__item select-text';
+    valueText.textContent = textValue;
+    valueText.id = idValue;
+    valuesUl.appendChild(valueText);
+    valueText.addEventListener('click', () => {
+        selectHeaderText.textContent = value.value;
+        isSelectBodyShow = true;
+        closeSelectBody(selectBody);
+        console.log(value);
+    })
+})
